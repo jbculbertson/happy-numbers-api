5963709cd38cc449efc2bfb89e73d94f4ba814d8
jbculbertson <jbculbertson@gmail.com>
2018-09-03 13:55:37 -0400

    Update port setting

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const cors = require('cors');
 const User = require('./models/user.js');
 const userController = require('./controllers/users.js');
 const authenticate = require('./controllers/authenticate.js');
+const port = process.env.PORT || 3000;
+
 function start() {
   const bodyparserOptions = { extended: true };
   app.use(cors());
@@ -16,7 +18,7 @@ function start() {
   app.post('/user', userController.addScore);
   app.post('/register', userController.create);
   app.post('/login', authenticate.post);
-  app.listen(3000, () => console.log('Crayon server listening on port 3000!'))
+  app.listen(port, () => console.log('Crayon server listening on port 3000!'))
 }
 
 const url = 'mongodb://jeff:Password21@ds141952.mlab.com:41952/crayon-happy'
